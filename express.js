3d0158fbb466b2faadb8882a587cdd2bdc514e71
Kat <kat@kattschan.co.uk>
2024-07-27 13:57:15 -0400

    converting to split express/svelte model

--- express.js
+++ express.js
@@ -1,14 +1,29 @@
 import express from 'express';
-import multer from 'multer';
+import fs from 'fs';
+const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+
 const app = express();
-const port = 3000;
+
+// Middleware to parse raw data
+app.use(express.raw({ type: () => true, limit: '5gb' }));
 
 app.put('/*', (req, res) => {
-	const file = req.body;
-	console.log('file ', file);
-	res.send('PUT request');
+	const fileName = req.path.slice(1);
+	if (!fileName) {
+		return res.status(400).send('No file name provided in the URL.');
+	}
+	const id = Math.random().toString(36).substr(2, 5);
+	fs.mkdirSync(`./files/${id}`, { recursive: true });
+	console.log(req.body);
+	const fileData = Buffer.isBuffer(req.body) ? req.body : Buffer.from(new Uint8Array(req.body));
+	fs.writeFile(`./files/${id}/${fileName}`, fileData, (err) => {
+		if (err) {
+			return res.status(500).send(err.message);
+		}
+		res.status(200).send(`${config.urlRoot}/${id}/${fileName}`);
+	});
 });
 
-app.listen(3000, () => {
-	console.log('Server is running on port 3000');
+app.listen(4000, () => {
+	console.log('Server is running on port 4000');
 });

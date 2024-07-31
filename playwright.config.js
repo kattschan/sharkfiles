5a0b95b1352d94512d3ca2f0ff20ed12b22c6100
Kat <kat@kattschan.co.uk>
2024-07-31 09:57:46 -0400

    further trim the devices

--- playwright.config.js
+++ playwright.config.js
@@ -9,27 +9,17 @@ export default defineConfig({
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
 	projects: [
-		/* Test against desktop browsers */
 		{
 			name: 'chromium',
-			use: { ...devices['Desktop Chrome'] }
-		},
-		{
-			name: 'firefox',
-			use: { ...devices['Desktop Firefox'] }
-		},
-		{
-			name: 'webkit',
-			use: { ...devices['Desktop Safari'] }
-		},
-		/* Test against mobile viewports. */
-		{
-			name: 'Mobile Chrome',
-			use: { ...devices['Pixel 5'] }
+			use: {
+				...devices['Desktop Chrome']
+			}
 		},
 		{
 			name: 'Mobile Safari',
-			use: { ...devices['iPhone 12'] }
+			use: {
+				...devices['iPhone 13']
+			}
 		}
 	]
 });
